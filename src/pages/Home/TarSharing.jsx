138b6b4dbafbe99e35443995333e71d00ce41771
minh1107 <minhdepzaiahihi@gmail.com>
2023-09-28 15:06:56 +0700

    update

--- src/pages/Home/TarSharing.jsx
+++ src/pages/Home/TarSharing.jsx
@@ -32,6 +32,7 @@ const TarSharing = () => {
     const handleSlideChange = (swiper) => {
       setCurrentSlideIndex(swiper.realIndex); // or swiper.activeIndex
     };
+    const newArr = Array.from(Array(10).keys())
   return (
     <div className='relative tar-sharing h-[38.25vw] max-md:h-[95vw]'>
         <Image src={bgImg} className='-z-50 absolute h-[40vw] top-0 left-0 w-screen max-md:h-[95vw]' alt='background' />
@@ -49,58 +50,23 @@ const TarSharing = () => {
                     <Image src={buttonLeft} className='w-[2.5vw] h-[2.5vw] mr-[1vw] max-md:hidden' alt='background' />
                 </button>
                 <Swiper onSwiper={handleSlideChange} navigation={true} modules={[Navigation]} className="mySwiper w-[44.8125vw] h-[20.125vw] info max-md:w-[69.96vw] max-md:h-[43.41vw]">
-                    <SwiperSlide>
-                    <div>
-                        <div className='ml-[3.38vw] mt-[2.5vw] flex max-md:mx-[7%] max-md:mt-[6.61vw] items-center mb-[2.25vw]'>
-                            <Image src={avatar} alt='' className='w-[5.5625vw] rounded-full h-[5.5625vw] max-md:w-[10.22vw] max-md:h-[10.22vw]'/>
-                            <div className='ml-[1.5vw]'>
-                                <h2 className='text-[1.25vw] text-[#EC4AE7]  font-bold max-md:text-[2.66vw] mb-[0.5vw]'>Chị ĐINH THU HOÀI</h2>
-                                <p className='text-[#A4DEFF] max-md:text-[2.13vw]'>Chuyên viên tuyển dụng </p>
-                                <p className='text-[#A4DEFF] max-md:text-[2.13vw]'>& Quản lý Nguồn nhân lực, PwC Việt Nam</p>
+                {
+                    newArr.map(item => (
+                        <SwiperSlide key={item}>
+                        <div key={-item}>
+                            <div className='ml-[3.38vw] mt-[2.5vw] flex max-md:mx-[7%] max-md:mt-[6.61vw] items-center mb-[2.25vw]'>
+                                <Image src={avatar} alt='' className='w-[5.5625vw] rounded-full h-[5.5625vw] max-md:w-[10.22vw] max-md:h-[10.22vw]'/>
+                                <div className='ml-[1.5vw]'>
+                                    <h2 className='text-[1.25vw] text-[#EC4AE7]  font-bold max-md:text-[2.66vw] mb-[0.5vw]'>Chị ĐINH THU HOÀI</h2>
+                                    <p className='text-[#A4DEFF] max-md:text-[2.13vw]'>Chuyên viên tuyển dụng </p>
+                                    <p className='text-[#A4DEFF] max-md:text-[2.13vw]'>& Quản lý Nguồn nhân lực, PwC Việt Nam</p>
+                                </div>
                             </div>
+                            <p className='ml-[3.38vw] mr-[3vw] text-white max-md:text-[2.4vw] max-md:mt-[6.4vw] max-md:mx-[7%]'>The Audit Race là một sân chơi học thuật được đánh giá cao bởi luôn giữ vững được tính chuyên môn cũng như chất lượng thí sinh tham gia thông qua sự giám sát và hỗ trợ từ phía các thầy cô, chuyên gia trong lĩnh vực.</p>
                         </div>
-                        <p className='ml-[3.38vw] mr-[3vw] text-white max-md:text-[2.4vw] max-md:mt-[6.4vw] max-md:mx-[7%]'>The Audit Race là một sân chơi học thuật được đánh giá cao bởi luôn giữ vững được tính chuyên môn cũng như chất lượng thí sinh tham gia thông qua sự giám sát và hỗ trợ từ phía các thầy cô, chuyên gia trong lĩnh vực.</p>
-                    </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                    <div>
-                        <div className='ml-[3.38vw] mt-[2.5vw] flex max-md:mx-[7%] max-md:mt-[6.61vw] items-center mb-[2.25vw]'>
-                            <Image src={avatar} alt='' className='w-[5.5625vw] rounded-full h-[5.5625vw] max-md:w-[10.22vw] max-md:h-[10.22vw]'/>
-                            <div className='ml-[1.5vw]'>
-                                <h2 className='text-[1.25vw] text-[#EC4AE7]  font-bold max-md:text-[2.66vw] mb-[0.5vw]'>Chị ĐINH THU HOÀI</h2>
-                                <p className='text-[#A4DEFF] max-md:text-[2.13vw]'>Chuyên viên tuyển dụng </p>
-                                <p className='text-[#A4DEFF] max-md:text-[2.13vw]'>& Quản lý Nguồn nhân lực, PwC Việt Nam</p>
-                            </div>
-                        </div>
-                        <p className='ml-[3.38vw] mr-[3vw] text-white max-md:text-[2.4vw] max-md:mt-[6.4vw] max-md:mx-[7%]'>The Audit Race là một sân chơi học thuật được đánh giá cao bởi luôn giữ vững được tính chuyên môn cũng như chất lượng thí sinh tham gia thông qua sự giám sát và hỗ trợ từ phía các thầy cô, chuyên gia trong lĩnh vực.</p>
-                    </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                    <div>
-                        <div className='ml-[3.38vw] mt-[2.5vw] flex max-md:mx-[7%] max-md:mt-[6.61vw] items-center mb-[2.25vw]'>
-                            <Image src={avatar} alt='' className='w-[5.5625vw] rounded-full h-[5.5625vw] max-md:w-[10.22vw] max-md:h-[10.22vw]'/>
-                            <div className='ml-[1.5vw]'>
-                                <h2 className='text-[1.25vw] text-[#EC4AE7]  font-bold max-md:text-[2.66vw] mb-[0.5vw]'>Chị ĐINH THU HOÀI</h2>
-                                <p className='text-[#A4DEFF] max-md:text-[2.13vw]'>Chuyên viên tuyển dụng </p>
-                                <p className='text-[#A4DEFF] max-md:text-[2.13vw]'>& Quản lý Nguồn nhân lực, PwC Việt Nam</p>
-                            </div>
-                        </div>
-                        <p className='ml-[3.38vw] mr-[3vw] text-white max-md:text-[2.4vw] max-md:mt-[6.4vw] max-md:mx-[7%]'>The Audit Race là một sân chơi học thuật được đánh giá cao bởi luôn giữ vững được tính chuyên môn cũng như chất lượng thí sinh tham gia thông qua sự giám sát và hỗ trợ từ phía các thầy cô, chuyên gia trong lĩnh vực.</p>
-                    </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                    <div>
-                        <div className='ml-[3.38vw] mt-[2.5vw] flex max-md:mx-[7%] max-md:mt-[6.61vw] items-center mb-[2.25vw]'>
-                            <Image src={avatar} alt='' className='w-[5.5625vw] rounded-full h-[5.5625vw] max-md:w-[10.22vw] max-md:h-[10.22vw]'/>
-                            <div className='ml-[1.5vw]'>
-                                <h2 className='text-[1.25vw] text-[#EC4AE7]  font-bold max-md:text-[2.66vw] mb-[0.5vw]'>Chị ĐINH THU HOÀI</h2>
-                                <p className='text-[#A4DEFF] max-md:text-[2.13vw]'>Chuyên viên tuyển dụng </p>
-                                <p className='text-[#A4DEFF] max-md:text-[2.13vw]'>& Quản lý Nguồn nhân lực, PwC Việt Nam</p>
-                            </div>
-                        </div>
-                        <p className='ml-[3.38vw] mr-[3vw] text-white max-md:text-[2.4vw] max-md:mt-[6.4vw] max-md:mx-[7%]'>The Audit Race là một sân chơi học thuật được đánh giá cao bởi luôn giữ vững được tính chuyên môn cũng như chất lượng thí sinh tham gia thông qua sự giám sát và hỗ trợ từ phía các thầy cô, chuyên gia trong lĩnh vực.</p>
-                    </div>
-                    </SwiperSlide>
+                        </SwiperSlide>
+                    ))
+                }
                 </Swiper>
                 <button onClick={changeSlideNext}>     
                     <Image src={buttonRight}  className='w-[2.5vw] h-[2.5vw] ml-[1vw] max-md:hidden' alt='background' />
