b5721f7d44a267874e3089c349229eaf925a19b3
minh1107 <minhdepzaiahihi@gmail.com>
2023-09-28 18:19:32 +0700

    update

--- src/pages/Home/Header.jsx
+++ src/pages/Home/Header.jsx
@@ -11,40 +11,40 @@ import Line2 from '@/components/common/Line2'
 import { usePathname } from 'next/navigation'
 import Navbar from './Navbar'
 import closeImg from '@/assets/images/svg/close.svg'
-import { useRouter } from 'next/navigation'
- 
+
 const Header = () => {
     const [show, setShow] = useState()
     const pathname = usePathname()
-    const router = useRouter()
-
-    const handleScroll = (e, index) => {
+    const handleScroll = (e) => {
         e.preventDefault();
-        if(index === 0) {
-            router.push('/')
-        } else {
-            const href = e.currentTarget.href;
-            const targetId = href.replace(/.*\#/, "");
-            const elem = document.getElementById(targetId);
-            elem?.scrollIntoView({
-              behavior: "smooth",
-            });
-        }
+        const href = e.currentTarget.href;
+        const targetId = href.replace(/.*\#/, "");
+        const elem = document.getElementById(targetId);
+        elem?.scrollIntoView({
+          behavior: "smooth",
+        });
         
       };
     return (
     <div className={`absolute z-50 top-0 header whitespace-nowrap overflow-x-hidden max-md:h-[14.6408vw] ${show && 'max-md:bg-[#01026D]'} max-md:fixed`}>
-        <div className=' flex text-main h-[5.94vw] items-center mx-[13.16vw] mr-[16.19vw] justify-between 
+        <div className=' flex text-main h-[5.94vw] items-center mx-[10.16vw] mr-[16.19vw] justify-between 
         max-md:items-center max-md:h-[14.6408vw] max-md:w-[calc(100vw-10.88vw)] max-md:mx-[5.44vw]'>
             <Link href={'/'}><Image src={bigLogo} alt='logo' className='w-auto h-[5.00894vw] mr-[2.5vw] max-md:w-[32.1104vw] max-md:h-[10.53vw]'/></Link>
             <ul className='flex gap-[0.25vw] mr-[1.5vw] max-md:hidden'>
-            {
-                headerPaths?.map((item, index) => (
-                    <Link onClick={() => handleScroll(index)} 
-                      href={index === 0 ? '' : item?.link} key={item?.id} className='text-[1vw] font-bold font-exoFont uppercase px-[1vw] '>
-                        {item?.name}
+            <Link 
+                      href={'/'} className='text-[1vw] font-bold font-exoFont uppercase px-[1vw] '>
+                        Trang chủ
                     </Link>
-                ))
+            {
+                headerPaths?.map((item, index) => {
+                    if(index !== 0)
+                    return (
+                        <Link onClick={handleScroll} 
+                          href={index === 0 ? '/' : item?.link} key={item?.id} className='text-[1vw] font-bold font-exoFont uppercase px-[1vw] '>
+                            {item?.name}
+                        </Link>
+                    )
+                })
             }
             </ul>
             {pathname !== '/form-register' && <Link className='max-md:hidden' href={'/form-register'}><ButtonCustom text={'Đăng ký dự thi'} /></Link>}
